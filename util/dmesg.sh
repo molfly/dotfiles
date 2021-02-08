221a467c8bc5eb49dc224e8152ecee5ccfcec1f2
Andrey Molfly <andrey.molfly@gmail.com>
2021-02-08 13:18:14 +0500

    add dmesc and timezone script

---
+++ util/dmesg.sh
@@ -0,0 +1,8 @@
+base=$(cut -d '.' -f1 /proc/uptime); 
+seconds=$(date +%s); 
+dmesg | sed 's/\]//;s/\[//;s/\([^.]\)\.\([^ ]*\)\(.*\)/\1\n\3/' | 
+while read first; do 
+  read second; 
+  first=`date +"%d/%m/%Y %H:%M:%S" --date="@$(($seconds - $base + $first))"`;
+  printf "[%s] %s\n" "$first" "$second"; 
+done 
